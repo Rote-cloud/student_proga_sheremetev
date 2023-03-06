4060f1f94d53979ed0c249917aa0c1e175c82362
Кирилл <pote1357@mail.ru>
2023-03-06 10:29:52 +0300

    commit

--- ribka.py
+++ ribka.py
@@ -15,7 +15,6 @@ for i in range(n):
 
 first_letter, last_letter = up_dict(int(input())), up_dict(int(input()))
 first_collec, last_collec = collections.Counter(first_word), collections.Counter(last_word)
-first_count, last_count = 0, 0
 word_count1 = sum([min(first_letter.get(i), first_collec.get(i)) for i in first_letter.keys() if first_collec.get(i) != None])
 word_count2 = sum([min(last_letter.get(i), last_collec.get(i)) for i in last_letter.keys() if last_collec.get(i) != None])
 print(min(word_count2, word_count1))
