5b7ec8a494a16fa627c354c18db55faddcad1223
Кирилл <pote1357@mail.ru>
2023-03-18 10:25:12 +0300

    commit

--- dijkstra.py
+++ dijkstra.py
@@ -13,20 +13,29 @@ arr = np.array([[0, 7, 0, 0, 9, 2, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 0, 0, 4, 8, 6, 10, 0, 3],
        [0, 0, 0, 0, 0, 0, 0, 5, 1, 0, 3, 0]])
 arr = np.where(arr == 0, max_int32, arr)
-n = int(input()) - 1
+n = int(input())
 array, array_s, m, koor_min, num_min = [n], {}, arr.shape[0], n, 0
 array_col = [i for i in range(arr.shape[0]) if i != n]
+print(array_col)
 while len(array) < arr.shape[0]:
        index_min_el = np.argmin(arr[koor_min, :])
+       print(arr[koor_min, array_col])
+       print(array_col, array)
        if len(array) > 1:
               for j in array_col:
-                     arr[koor_min, j] = min(num_min + arr[koor_min, j], arr[n, j])
-       array_col.remove(index_min_el)
+                     arr[koor_min, j] = min(array_s.get(koor_min) + arr[koor_min, j], arr[n, j])
+              print(arr[koor_min, :])
+       #print(index_min_el)
+       #print(arr[index_min_el, :])]
+       print(index_min_el)
        num_min = arr[koor_min, index_min_el]
-       array_s[index_min_el + 1] = arr[koor_min, index_min_el]
+       array_s[index_min_el] = arr[koor_min, index_min_el]
        arr[koor_min, index_min_el], arr[index_min_el, koor_min] = max_int32, max_int32
        if index_min_el not in array:
               array.append(index_min_el)
+              array_col.remove(index_min_el)
+
        koor_min = index_min_el
-print(array_s)
+       n = koor_min
+print(dict(sorted(array_s.items())))
 print(array_s[int(input())])
