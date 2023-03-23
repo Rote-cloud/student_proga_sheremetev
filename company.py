1dc1d2767d5ba5c42976554d502f63e978f2589a
Кирилл <pote1357@mail.ru>
2023-03-23 20:04:49 +0300

    commit

--- company.py
+++ company.py
@@ -1,5 +1,4 @@
 worker = {}
-#employees = {}
 
 class Worker:
     def __init__(self, worker=[], name="Unknown Name"):
@@ -16,14 +15,6 @@ class Worker:
         self.worker.append(worker)
 
 
-def addition(string, dict):
-    if len(string) > 1:
-        dict[string[0]] = string[1]
-    else:
-        if string[0] not in employees.keys():
-            employees[string[0]] = "Unknown Name"
-    return dict
-
 def tree(dict, id):
     try:
         id_infor = dict[id]
@@ -38,7 +29,6 @@ while True:
     subordinates = input()
     if boss[0] == "END":
         if not subordinates.isdigit():
-            #subordinates = list(employees.keys())[list(employees.values()).index(subordinates)]
             for id, item in worker.items():
                 if item.getName() == subordinates:
                     subordinates = id
@@ -57,13 +47,3 @@ while True:
                 worker[boss[0]] = Worker([subordinates])
         else:
             worker[boss[0]].addWorker(subordinates)
-
-
-    #addition(boss, employees)
-    #addition(subordinates, employees)
-
-
-
-
-# дана строка которая включает в себя 3 вида скобок, определить правильно ли раставлены скобки (), [], {}
-# sortlist, list, словарь, очередь, стек,
