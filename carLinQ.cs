9b1c057225113df78728a9c60e201c337b257a2e
Rote-cloud <85827294+Rote-cloud@users.noreply.github.com>
2023-05-31 16:45:07 +0300

    Update carLinQ.cs

--- carLinQ.cs
+++ carLinQ.cs
@@ -5,47 +5,60 @@ using System.Linq;
 
 class Car
 {
-    public int Id { get; set; } 
+    public int Id { get; set; }
     public string DriverName { get; set; }
     public string DriverSurename { get; set; }
+}
+
+class Driver
+{
+    public string DriverName { get; set; }
+    public string DriverSurename { get; set; }
+    public int CarNumber { get; set; }
 }
 
 public class Program
 {
     static void Main()
     {
-        List<Car> drivers = new List<Car>
+        List<Car> cars = new List<Car>
             {
-                new Car{ DriverName = "Vyacheslav", DriverSurename = "Izengardovich", Id = 3},
-                new Car{ DriverName = "Ivan", DriverSurename = "Ivanov", Id = 1},
-                new Car{ DriverName = "San", DriverSurename = "Sanich", Id = 2},
-                new Car{ DriverName = "Michael", DriverSurename = "Jackeson", Id = 1},
-                new Car{ DriverName = "Janna", DriverSurename = "D'Arck", Id = 3}
+                new Car{Id = 1 },
+                new Car{Id = 2 },
+                new Car{Id = 3 },
+            };
+        List<Driver> drivers = new List<Driver>
+            {
+                new Driver{ DriverName = "Vyacheslav", DriverSurename = "Izengardovich", CarNumber = 3},
+                new Driver{ DriverName = "Ivan", DriverSurename = "Ivanov", CarNumber = 1},
+                new Driver{ DriverName = "San", DriverSurename = "Sanich", CarNumber = 2},
+                new Driver{ DriverName = "Michael", DriverSurename = "Jackeson", CarNumber = 1},
+                new Driver{ DriverName = "Janna", DriverSurename = "D'Arck", CarNumber = 3},
             };
 
-        foreach (Car i in from p in drivers
-                          where p.Id == 1
+        foreach (Driver i in from p in drivers
+                          where p.CarNumber == 1
                           select p)
         {
             Console.Write(i.DriverSurename + " ");
         }
         Console.WriteLine();
 
-        foreach (Car i in from p in drivers
+        foreach (Driver i in from p in drivers
                           where p.DriverSurename == "Izengardovich"
                           select p)
         {
-            Console.Write(i.Id + " ");
+            Console.Write(i.CarNumber + " ");
         }
         Console.WriteLine();
 
-        foreach (Car i in from p in drivers
+        foreach (Driver i in from p in drivers
                           where p.DriverSurename.StartsWith("I")
                           select p)
         {
             Console.WriteLine(i.DriverName + " " + i.DriverSurename);
         }
         Console.ReadLine();
-        
+
     }
 }
